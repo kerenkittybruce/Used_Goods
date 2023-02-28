faae90a3dbdeb66c7180290607cab5994ea5ac76
Keren Bruce <kerenchelseabruce@gmail.com>
2023-02-28 13:16:28 +0200

    Added index.html inside View directory

--- controller/index.js
+++ controller/index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const route = express.Router();
 const { User, Product } = require("../model");
 
